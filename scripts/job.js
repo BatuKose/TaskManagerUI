a1f4e3442e2faa75f061ec6dde70a3fa3dcd0b7d
Batuhan Kose <batuhankose36@gmail.com>
2026-03-05 22:42:58 +0300

    Job UI api işlemleri step 2

--- scripts/job.js
+++ scripts/job.js
@@ -14,19 +14,44 @@ document.addEventListener("DOMContentLoaded",async()=>
 {
     BütünIsleriGetirAsync () 
 });
-async function BütünIsleriGetirAsync () {
-    try
-    {
-        const istek= await fetch(`http://localhost:1000/api/Job/Bütünisler`);
-    const data= await istek.json();
-    console.log(data);
-    if(!istek.ok)
-    {
-       throw new Error(await data.text());
+async function BütünIsleriGetirAsync() {
+    try {
+        const istek = await fetch(`http://localhost:1000/api/Job/Bütünisler`);
+        const data = await istek.json();
+
+       if (!istek.ok) {
+    console.log(Object.keys(data));
+    throw new Error(data.Message || data.message || "Bilinmeyen hata");
+}
+
+        console.log(data);
+        fillTableJobAll(data);
     }
+    catch (err) {
+        alert(err.message);
     }
-    catch(err)
-    {
-        alert(err.message)
-    }
+}
+function fillTableJobAll(data)
+{
+    const tablo=document.querySelector("#BütünIsler tbody");
+    tablo.innerHTML="";
+    data.forEach(job => {
+        const tr=document.createElement("tr");
+        tr.innerHTML=`
+            <td>${job.jobHeaderId}</td>
+            <td>${job.jobHeaderName}</td>
+            <td>${job.jobDetayName}</td>
+            <td>${job.managerUserName}</td>
+            <td>${job.managerRole}</td>
+            <td>${job.workerUserName}</td>
+            <td>${job.workerRole}</td>
+            <td>${job.workCreateTıme}</td>
+            <td>${job.deadline}</td>
+            <td>${job.jobFinishedTime}</td>
+            <td>${job.jobDetayStatus}</td>
+            <td>${job.jobHeaderStatus}</td>
+        `; 
+         tablo.appendChild(tr);  
+    });
+
 }
